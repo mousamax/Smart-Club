543c972a42c9a816c38fae4ac2236d9646a4b132
ahmedehabb <58532236+ahmedehabb@users.noreply.github.com>
2020-12-25 23:48:23 +0200

    Add files via upload

---
+++ Gui/GUIPROJECT/GUIPROJECT/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIPROJECT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CustomizeDesign();
+        }
+
+        private void CustomizeDesign()
+        {
+            FirstPanel.Visible = false;
+            SecondPanel.Visible = false;
+            ThirdPanel.Visible = false;
+        }
+
+        private void hideSubMenu()
+        {
+            if (FirstPanel.Visible == true)
+                FirstPanel.Visible = false;
+
+            if (SecondPanel.Visible == true)
+                SecondPanel.Visible = false;
+            if (ThirdPanel.Visible == true)
+                ThirdPanel.Visible = false;
+        }
+
+        private void ShowSubmenu(Panel Submenu)
+        {
+            if (Submenu.Visible == false)
+            {
+                hideSubMenu();
+                Submenu.Visible = true;
+            }
+            else
+                Submenu.Visible = false;
+
+           
+        }
+
+        #region FirstSubmenu
+        private void FirstSubmenubutton_Click(object sender, EventArgs e)
+        {
+            ShowSubmenu(FirstPanel);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+            testform x = new testform();
+            openChildForm(x);
+            hideSubMenu();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+
+            hideSubMenu();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+
+            hideSubMenu();
+        }      
+#endregion
+
+        #region SecondSubmenu
+
+        private void SecondSubButton_Click(object sender, EventArgs e)
+        {
+            ShowSubmenu(SecondPanel);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+
+            hideSubMenu();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+
+            hideSubMenu();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+
+            hideSubMenu();
+        }
+        #endregion
+
+        #region ThirdSubmenu
+
+        private void ThirdSubButton_Click(object sender, EventArgs e)
+        {
+            ShowSubmenu(ThirdPanel);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+
+            hideSubMenu();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+
+            hideSubMenu();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //code showing form or whatever
+
+            hideSubMenu();
+        }
+
+        #endregion
+
+        private Form ActiveForm = null;
+        private void openChildForm(Form ChildForm)
+        {
+            if (ActiveForm != null)
+                ActiveForm.Close();
+            ActiveForm = ChildForm;
+            ChildForm.TopLevel = false;
+            ChildForm.FormBorderStyle = FormBorderStyle.None;
+            ChildForm.Dock = DockStyle.Fill;
+            panelchildform.Controls.Add(ChildForm);
+            panelchildform.Tag = ChildForm;
+            ChildForm.BringToFront(); // because we have a logoo
+            ChildForm.Show();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            if(ActiveForm!=null)
+                ActiveForm.Close();
+            this.Close();
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            LOGIN x = new LOGIN();
+            openChildForm(x);
+          
+        }   
+
+
+
+    }
+}
---
+++ Gui/GUIPROJECT/GUIPROJECT/LOGIN.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace GUIPROJECT
+{
+    partial class LOGIN
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LOGIN));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Location = new System.Drawing.Point(207, 162);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Location = new System.Drawing.Point(207, 222);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(213, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Location = new System.Drawing.Point(207, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Sign in";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button2.Location = new System.Drawing.Point(207, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Register";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(235, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(162, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LOGIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(30)))), ((int)(((byte)(45)))));
+            this.ClientSize = new System.Drawing.Size(677, 572);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LOGIN";
+            this.Text = "LOGIN";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
