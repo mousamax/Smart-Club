5e3dcb7a3def4273ed395d03f5b403bad9b323ab
Mustafa Usama <57876795+mustafausamaa@users.noreply.github.com>
2021-01-01 21:49:31 +0200

    Add files via upload

---
+++ GUIPROJECT/GUIPROJECT/GUIPROJECT/GUIPROJECT/MemberController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace GUIPROJECT
+{
+    class MemberController
+    {
+        DBManager dbMan;
+        public MemberController()
+        {
+            dbMan = new DBManager();
+        }
+
+
+        public void TerminateConnection()
+        {
+            dbMan.CloseConnection();
+        }
+        public int Addevent(string name,string place,string date, int fees, int memberid)
+        {
+            string query = "Insert into Events values('"+name+"','"+place+"','"+date+"',null,"+fees+","+memberid+")";
+            return dbMan.ExecuteNonQuery(query);
+        }
+
+        public int ParkingSubscribe(string startdate,int fees, string enddate, int memberid)
+        {
+            string query = "Insert into Parking values('" + startdate + "'," + fees + ",'" + enddate + "',"  + memberid + ")";
+            return dbMan.ExecuteNonQuery(query);
+        }
+    }
+}
